e539d3ce9deef80c6f13a7f533754f2dd3166d78
Giuseppe Castellucci <castellucci.giuseppe@gmail.com>
2015-01-02 12:54:04 +0100

    first evaluator versions

---
+++ src/main/java/it/uniroma2/sag/kelp/utils/evaluation/AccuracyEvaluator.java
@@ -0,0 +1,71 @@
+package it.uniroma2.sag.kelp.utils.evaluation;
+
+import it.uniroma2.sag.kelp.data.label.Label;
+/**
+ * This is an instance of an Evaluator. 
+ * It allows to compute the Accuracy.
+ * 
+ * @author Giuseppe Castellucci
+ */
+public class AccuracyEvaluator extends Evaluator {
+	private int correct;
+	private int total;
+	
+	private float accuracy;
+	
+	/**
+	 * Initialize a new F1Evaluator that will work on the specified classes
+	 * @param labels
+	 */
+	public AccuracyEvaluator() {
+		correct=0;
+		total=0;
+		accuracy=0.0f;
+	}
+
+	public void addCount(Label gold, Label predicted) {
+		total++;
+		if (gold.equals(predicted))
+			correct++;
+	}
+
+	public void compute() {
+		if (total!=0) {
+			accuracy = (float)correct/(float)total;
+		}
+	}
+
+	/**
+	 * Return the mean of the F1 scores considering all the labels involved
+	 * 
+	 * @return mean F1 of all Label{s}
+	 */
+	public float getAccuracy() {
+		return accuracy;
+	}
+
+	/**
+	 * Clear all the counters for a new processing.
+	 */
+	public void clear() {
+		correct=0;
+		total=0;
+		accuracy=0.0f;
+	}
+	
+	/**
+	 * Print the counters in a human-readable format
+	 */
+	@SuppressWarnings("unused")
+	private void printCounters() {
+		System.out.println("Total: " + total+"\n");
+		System.out.println("Correct: " + correct+"\n");
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder b = new StringBuilder();
+		b.append("Accuracy: " + accuracy+"\n");
+		return b.toString().trim();
+	}
+}
---
+++ src/main/java/it/uniroma2/sag/kelp/utils/evaluation/Evaluator.java
@@ -0,0 +1,39 @@
+package it.uniroma2.sag.kelp.utils.evaluation;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import it.uniroma2.sag.kelp.data.label.Label;
+import it.uniroma2.sag.kelp.utils.exception.NoSuchPerformanceMeasureException;
+
+public abstract class Evaluator {
+	public float getPerformanceMeasure(String performanceMeasureMethodName, Object[] args) throws NoSuchPerformanceMeasureException {
+		this.compute();
+		Class<?>[] methodParameters=null;
+		if (args != null) {
+			methodParameters = new Class<?>[args.length];
+			for (int i=0; i<args.length; ++i) {
+				methodParameters[i] = args[i].getClass();
+			}
+		}
+		try {
+			Method method = this.getClass().getMethod(performanceMeasureMethodName, methodParameters);
+			Object invokedResult = method.invoke(this, args);
+			Float res = (Float)invokedResult;
+			return res.floatValue();
+		} catch (SecurityException e) {
+			throw new NoSuchPerformanceMeasureException("Evaluator can't find the specified performance measure: " + performanceMeasureMethodName);
+		} catch (NoSuchMethodException e) {
+			throw new NoSuchPerformanceMeasureException("Evaluator can't find the specified performance measure: " + performanceMeasureMethodName);
+		} catch (IllegalArgumentException e) {
+			throw new NoSuchPerformanceMeasureException("Evaluator can't call the specified performance measure: " + performanceMeasureMethodName + " with the specified arguments");
+		} catch (IllegalAccessException e) {
+			throw new NoSuchPerformanceMeasureException("Evaluator can't call the specified performance measure: " + performanceMeasureMethodName + " with the specified arguments");
+		} catch (InvocationTargetException e) {
+			throw new NoSuchPerformanceMeasureException("Evaluator can't call the specified performance measure: " + performanceMeasureMethodName + " with the specified arguments");
+		}
+	}
+	
+	public abstract void addCount(Label gold, Label predicted);
+	public abstract void compute();
+}
